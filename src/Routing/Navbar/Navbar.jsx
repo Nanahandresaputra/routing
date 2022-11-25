95fcc19bab143aa459d9f28c54206317f15a6e67
Nanahandresaputra <nanaggmu@gmail.com>
2022-11-25 14:27:44 +0700

    23

--- src/Routing/Navbar/Navbar.jsx
+++ src/Routing/Navbar/Navbar.jsx
@@ -1,22 +1,22 @@
 // import { Link } from "react-router-dom";
 import "./Navbar.css";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faHome, faAddressBook, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faAddressBook, faUser } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
     <div className="navigation">
       <Link to="/" className="link">
-        {/* <FontAwesomeIcon icon={faHome} /> */}
+        <FontAwesomeIcon icon={faHome} />
         Home
       </Link>
       <Link to="/contact" className="link">
-        {/* <FontAwesomeIcon icon={faAddressBook} /> */}
+        <FontAwesomeIcon icon={faAddressBook} />
         Contact
       </Link>
       <Link to="/user" className="link">
-        {/* <FontAwesomeIcon icon={faUser} /> */}
+        <FontAwesomeIcon icon={faUser} />
         User
       </Link>
     </div>
